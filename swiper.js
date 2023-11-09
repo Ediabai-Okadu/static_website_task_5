8af966ad8d3194b25d42bb40c92592ec7a230835
Bright_Bai <okaduediabai@gmail.com>
2023-11-09 01:02:31 +0100

    This is a static website designed with the aid of bootstrap framework.

--- swiper.js
+++ swiper.js
@@ -1,34 +1,3 @@
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   direction: "horizontal ",
-//   loop: true,
-
-//   speed: 400,
-//   spaceBetween: 100,
-
-//   a11y: {
-//     prevSlideMessage: "Previous slide",
-//     nextSlideMessage: "Next slide",
-//   },
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
-// });
-
 var swiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 5000,
